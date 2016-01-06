74650a548531c4e0a32e6375b542dc86cd442820
Petr Devaikin <p.devaikin@gmail.com>
2016-01-06 14:01:05 +0100

    d3 projection used

--- web/source/js/drawing/map.js
+++ web/source/js/drawing/map.js
@@ -1,5 +1,10 @@
 define(['lib/d3', 'constants', 'interaction/events'], function(d3, constants, events) {
     var container = d3.select('.m-map');
+    var projection = d3.geo.mercator();
+    projection
+        .center([-0.084685, 51.529396])
+        .translate([250, 250])
+        .scale(400000);
 
     var zoomX = 10000,
         zoomY = 10000;
@@ -12,26 +17,17 @@ define(['lib/d3', 'constants', 'interaction/events'], function(d3, constants, ev
     function drawData(data) {
         console.log('Map: start');
 
-        var maxX = 0,
-            maxY = 0,
-            minX = 100000,
-            minY = 100000;
-
         container
             .selectAll('.m-map__photo')
                 .data(data)
             .enter().append('div')
                 .classed('m-map__photo', true)
-                .style('left', function(d) { return ((d.longitude + 0.084685) * zoomX + 250) + 'px'; })
-                .style('top', function(d) { return ((d.latitude - 51.529396) * zoomY + 250) + 'px'; })
+                .style('left', function(d) { return projection([d.longitude, d.latitude])[0] + 'px'; })
+                .style('top', function(d) { return projection([d.longitude, d.latitude])[1] + 'px'; })
                 .attr("longitude", function(d) { return d.longitude; })
                 .attr("latitude", function(d) { return d.latitude; })
                 .style('background', function(d) { return d.main_color; })
                 .on('mouseover', events.photoHover);
-
-        console.log(minX + ' - ' + maxX);
-        console.log(minY + ' - ' + maxY);
-        console.log('Map: done');
     }
 
     function draw(data) {
