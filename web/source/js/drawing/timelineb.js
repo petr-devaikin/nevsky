b56fe17d34b372378105d72d9531d41f3bb9412b
lipcanu <diana.lipcanu@gmail.com>
2016-01-15 17:40:06 +0100

    can't show a miserable scale :(
    when running the code,  the forEach within the drawData function in
    timelineb.js gives some sort of error: d3.min.js?v=1452875377755:1
    Uncaught TypeError: t.slice is not a function .
    
    I've been trying to past together stuff from tutorials, and examples,
    and that's why it's such a mess. and doesn't really do anything yet.

--- web/source/js/drawing/timelineb.js
+++ web/source/js/drawing/timelineb.js
@@ -1,35 +1,47 @@
 define(['lib/d3', 'constants', 'interaction/events'], function(d3, constants, events) {
-    // var container = d3.select('.m-timeline-b');
- function drawData(data) {
-    var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  var container = d3.select('.m-timeline-b');
 
-var parseDate = d3.time.format("%Y%m%d").parse;
+  
 
-var x = d3.time.scale()
-    .range([0, width]);
+  function drawData(data) {
+    //Width and height
+
+            var margin = {top: 20, right: 80, bottom: 30, left: 50},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+
+            var parseDate = d3.time.format("%Y%m%d").parse;
+
+             data.forEach(function(d) {
+                d.date = parseDate(d.date);
+            });
+            
+            //Create scale functions
+
+            var  xScale= d3.time.scale()
+                                .domain(d3.extent(data, function(d) { return d.date; }))
+                                .range([0, width]);    
+             
+            //Define X axis
+            var xAxis = d3.svg.axis()
+                              .scale(xScale)
+                              .orient("bottom");
+
+            //Create SVG element
+            var svg = container
+                        .append("svg")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+            //Create X axis
+            svg.append("g")
+                .call(xAxis);
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.date); });
+    }
 
-var svg = d3.select(".m-timeline-b").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-  });
-   
-}
     function draw(data) {
         drawData(data);
     }
--- web/source/js/main.js
+++ web/source/js/main.js
@@ -2,9 +2,9 @@ define(['lib/d3', 'drawing/palette', 'drawing/timeline', 'drawing/map', 'drawing
         function(d3, drawingPalette, drawingTimeline, drawingMap, drawingTimelineB) {
     return function() {
         d3.json("js/photos.json?v=" + (new Date()).getTime(), function(data) {
-            drawingPalette.draw(data);
-            drawingTimeline.draw(data);
-            drawingMap.draw(data);
+            // drawingPalette.draw(data);
+            // drawingTimeline.draw(data);
+            // drawingMap.draw(data);
             drawingTimelineB.draw(data);
         });
     }
