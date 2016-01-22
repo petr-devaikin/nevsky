a0b8090dc520089910d5262fdeac90283aaa0dcc
lipcanu <diana.lipcanu@gmail.com>
2016-01-22 22:54:01 +0100

    last save
    nothing interesting

--- web/source/js/drawing/timelineb.js
+++ web/source/js/drawing/timelineb.js
@@ -33,6 +33,39 @@ define(['lib/d3', 'constants', 'interaction/events'], function(d3, constants, ev
                         .append("g")
                         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+            var days = [];
+
+            for (var i = 0; i < 365; i++){
+                days[i] = [];
+            }
+
+        var zero = new Date(2015, 0, 1, 0, 0, 0, 0);
+//puts the data into  days[], ordered by days, each number represents a day of the year
+        for (var i = 0; i < data.length; i++) {
+            var timeDiff = data[i].date * 1000 - zero.getTime();
+            var diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+
+            days[diffDays].push(data[i]);
+            console.log(days[diffDays]);
+        }
+
+        //creates the div that contains the day square (X axis)
+        var days = container
+            .selectAll('.m-timeline__day')
+                .data(days)
+            .enter().append('div')
+                .classed('m-timeline__day', true)
+                .style('bottom', 500)
+                .style('left', function(d,i) {return i + 3;} );
+//creates the div that contains the color (y axis)
+        days
+            .selectAll('.m-timeline__day__photo')
+                .data(function(d) { return d; })
+            .enter().append('div')
+                .classed('m-timeline__day__photo', true)
+                .style('background', function (d) { return d.main_color; })
+                .style()
+                .on('mouseover', events.photoHover);
             //Create X axis
             svg.append("g")
                 .call(xAxis);
