b53aeb413e7d67b12aaa31916e0c6fd6119ecb54
lipcanu <diana.lipcanu@gmail.com>
2016-01-23 14:48:58 +0100

    Merge remote-tracking branch 'refs/remotes/origin/dianas-dev'
    # Conflicts:
    #	web/source/js/drawing/timeline.js
    #	web/source/js/main.js

--- web/source/js/main.js
+++ web/source/js/main.js
@@ -1,10 +1,10 @@
-define(['lib/d3', 'drawing/palette', 'drawing/timeline', 'drawing/map', 'drawing/timelineb'],
-        function(d3, drawingPalette, drawingTimeline, drawingMap, drawingTimelineB) {
+define(['lib/d3', 'drawing/palette', 'drawing/map', 'drawing/timelineb'],
+        function(d3, drawingPalette, drawingMap, drawingTimelineB) {
     return function() {
         d3.json("js/photos.json?v=" + (new Date()).getTime(), function(data) {
-            // drawingPalette.draw(data);
+            drawingPalette.draw(data);
             // drawingTimeline.draw(data);
-            // drawingMap.draw(data);
+            drawingMap.draw(data);
             drawingTimelineB.draw(data);
         });
     }
